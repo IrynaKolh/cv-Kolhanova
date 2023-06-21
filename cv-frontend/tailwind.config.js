acd892abd3b732d2e89a02ebde9e83337e1b182c
Iryna <iryna.kolhanova@gmail.com>
2023-06-20 23:04:23 -0400

    fix: favicon

--- cv-frontend/tailwind.config.js
+++ cv-frontend/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/assets/**.{jpg}"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/assets/**.{jpg}'],
   theme: {
     extend: {},
   },
-  plugins: [], 
-}
-
+  plugins: [],
+};
